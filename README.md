c96c6ba3700e4c0825520b4e4cd8ebf8c9491bc1
@whitep4nth3r <52798353+whitep4nth3r@users.noreply.github.com>
2022-12-14 11:42:03 +0000

    :sparkles: Create ring nav and add join link

--- README.md
+++ README.md
@@ -4,11 +4,60 @@
 
 Add the following to a page on your website.
 
+The fallback content is provided in case JavaScript isn't available. Feel free to omit this or edit this as you wish.
+
 ```html
 <script src="https://the-claw-webring-widget.netlify.app/the-claw-webring-widget.mjs" type="module"></script>
 
 <the-claw-webring-widget>
-  <!-- TO DO ADD FALLBACK CONTENT -->
+ <!-- start optional fallback content in the case of no JavaScript -->
+  <style>
+    .tcwr__inner {
+      color: inherit;
+      font-family: system-ui;
+      padding: 1rem;
+      font-size: 1rem;
+    }
+
+    .tcwr__header {
+      display: grid;
+      gap: 0.5rem 1rem;
+      align-items: center;
+      margin-bottom: 1rem;
+      justify-content: flex-start;
+      grid-template-areas: "image title" "image view";
+    }
+
+    .tcwr__title {
+      grid-area: title;
+      font-size: 1.4rem;
+      margin: 0;
+    }
+
+    .tcwr__image {
+      grid-area: image;
+      height: 4rem;
+      transform: rotate(-8deg);
+    }
+
+    .tcwr__view {
+      grid-area: view;
+      margin: 0;
+      color: inherit;
+    }
+  </style>
+  <div class="tcwr__inner">
+    <div class="tcwr__header">
+      <img
+        src="https://the-claw-webring.netlify.app/img/theclaw.png"
+        alt="The Claw Webring"
+        class="tcwr__image"
+      />
+      <h2 class="tcwr__title">The Claw Webring</h2>
+      <a href="https://github.com/whitep4nth3r/the-claw-webring" class="tcwr__view">View on GitHub</p>
+    </div>
+  </div>
+  <!-- end optional fallback content in the case of no JavaScript -->
 </the-claw-webring-widget>
 ```
 
@@ -25,6 +74,6 @@ Specify the theme on the Web Component like so:
 
 ```html
 <the-claw-webring-widget theme="light">
-  <!-- TO DO ADD FALLBACK CONTENT -->
+  <!-- ... -->
 </the-claw-webring-widget>
 ```
--- src/index.js
+++ src/index.js
@@ -12,17 +12,15 @@ function getRandomMember(members) {
   const randomMember = getRandomEntry(members);
 
   return `
-    <a href="${randomMember.url}" class="tcww__random">I'm feeling lucky</a>
+    <a href="${randomMember.url}" class="tcwr__navItem">Random</a>
   `;
 }
 
 function makeMemberList(members) {
-  const shuffledMembers = members.sort(() => (Math.random() > 0.5 ? 1 : -1));
-
-  return shuffledMembers
+  return members
     .map(
-      (member) => `<li class="tcww__membersListItem">
-        <a href="${member.url}" class="tcww__membersListItemLink">
+      (member) => `<li class="tcwr__membersListItem">
+        <a href="${member.url}" class="tcwr__membersListItemLink">
           ${member.name}
         </a>
       </li>`,
@@ -38,21 +36,44 @@ class TheClawWebringWidget extends HTMLElement {
 
   async connectedCallback() {
     const members = await fetch("https://the-claw-webring.netlify.app/data/members.json").then((res) => res.json());
+    const loc = document.location.href;
+
+    // // For testing purposes in development
+    // if (loc === "http://localhost:8888/") {
+    //   members.push({
+    //     url: "http://localhost:8888/",
+    //     name: "Testing in Dev",
+    //     feed: null,
+    //   });
+    // }
 
     const meta = await fetch("https://the-claw-webring.netlify.app/data/meta.json").then((res) => res.json());
 
+    const thisMemberIndex = members.map((e) => e.url).indexOf(loc);
+
+    const prevIndex = thisMemberIndex === 0 ? members.length - 1 : thisMemberIndex - 1;
+    const nextIndex = thisMemberIndex === members.length - 1 ? 0 : thisMemberIndex + 1;
+
+    const prevUrl = members[prevIndex].url;
+    const nextUrl = members[nextIndex].url;
+
     const html = `
-    <div class="tcww">
-      <div class="tcww__inner">
-        <div class="tcww__header">
-          <img src="${meta.image}" alt="${meta.title}" class="tcww__image" />
-          <h3 class="tcww__title">${meta.title}</h3>
-          <p class="tcww_count">${members.length} members</p>
+    <div class="tcwr">
+      <div class="tcwr__inner">
+        <div class="tcwr__header">
+          <img src="${meta.image}" alt="${meta.title}" class="tcwr__image" />
+          <h2 class="tcwr__title">${meta.title}</h2>
+          <p class="tcwr_count">${members.length} members</p>
+          <a href="https://github.com/whitep4nth3r/the-claw-webring#join-the-claw-webring" class="tcwr_join">Join The Claw Webring</a>
         </div>
 
-        ${getRandomMember(members)}
-        
-        <ul class="tcww__membersList">
+        <div class="tcwr__nav">
+          <a href=${prevUrl} class="tcwr__navItem">Prev</a>
+          ${getRandomMember(members)}
+          <a href="${nextUrl}" class="tcwr__navItem">Next</a>
+        </div>
+
+        <ul class="tcwr__membersList">
           ${makeMemberList(members)}
         </ul>
       </div>
